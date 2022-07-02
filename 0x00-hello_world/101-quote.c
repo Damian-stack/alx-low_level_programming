254e10271af2d2fb686252a48ed666aebb7ae669
Damian-stack <damianod002@gmail.com>
2022-07-02 10:04:38 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
+#include<unistd.h>
+
 /**
  * main - prints out the last part of a quote in the standard errror
  *
  * Return: 1 (success)
  */
+
 int main(void)
 {
-	char var[] = "and that piece of art is useful\" - Dora korpas, 2015-10-19\n";
-		write(2, var, sizeof(var) - 1);
+	char var[] = "and that piece of art is useful\" - Dora Korpas, 2015-10-19\n";
+
+	write(2, var, sizeof(var) - 1);
 	return (1);
 }
