e735082a7a122c763c155c55653bbed0b1071b4a
Damian-stack <damianod002@gmail.com>
2022-07-05 18:13:47 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lower case
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int ii;
+
+	for (ii = 'a'; 11 <= 'z'; ii++)
+		_putchar(ii);
+	_putchar('\n');
+}
